6f78b191a01287a162df502857a4f848353b9233
Maniprakash <tmanipra@gmail.com>
2024-08-05 20:54:47 +0100

    updated from local

--- autoflowx_dbt/models/autoflowx.sql
+++ autoflowx_dbt/models/autoflowx.sql
@@ -1,8 +1,5 @@
--- models/autoflowx.sql
+{{ config(
+    materialized='incremental'
+) }}
 
-select
-    current_date() as load_date,
-    current_timestamp() as load_time,
-    'sample_file.csv' as file_name
-from
-    `global-sign-431120-i5.autoflowx_landing.autoflowx`
+select * from  `global-sign-431120-i5.autoflowx_landing.autoflowx`
