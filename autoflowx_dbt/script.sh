df80d8abfc370f499b38f48dddcc112d3f9d6674
tmanipra <53463040+tmanipra@users.noreply.github.com>
2024-08-15 13:23:53 +0100

    Update script.sh

--- autoflowx_dbt/script.sh
+++ autoflowx_dbt/script.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 dbt --select autoflowx --profiles-dir . --target dev
+
+# Start a simple HTTP server to keep the container alive and comply with Cloud Run's requirements
+PORT=${PORT:-8080}  # Default to 8080 if PORT is not set
+
+# Use a simple HTTP server in Python to listen on the specified port
+python3 -m http.server $PORT
